1718a73d1124a3310396992025a8af2acdfbe735
David Landry <davidlandry93@gmail.com>
2024-07-16 11:19:18 +0200

    Fix bold bug

--- .eleventy.js
+++ .eleventy.js
@@ -22,7 +22,12 @@ module.exports = function (eleventyConfig) {
     });
 
 
-    eleventyConfig.addPassthroughCopy({ "_static/": "/" });
+    eleventyConfig.addPassthroughCopy({ 
+        "_static/": "/",
+        "node_modules/photoswipe/dist/photoswipe-lightbox.esm.min.js": "assets/photoswipe-lightbox.esm.min.js",
+        "node_modules/photoswipe/dist/photoswipe.esm.min.js": "assets/photoswipe.esm.min.js",
+        "node_modules/photoswipe/dist/photoswipe.css": "assets/photoswipe.css"
+     });
 
     return {
         dir: {
