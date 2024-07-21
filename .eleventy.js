7bb22a29b36e1aaa22672be97bcbbcd5fa21dda7
David Landry <davidlandry93@gmail.com>
2024-07-21 15:45:25 +0200

    Add gallery

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,71 @@
+const Image = require('@11ty/eleventy-img');
+const { parse } = require("csv-parse/sync");
+
 const { DateTime } = require('luxon')
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function (eleventyConfig) {
-
     eleventyConfig.addPlugin(pluginRss);
 
-    // Filters
+
+    eleventyConfig.addDataExtension("csv", (contents) => {
+        const parsed = parse(contents, {
+            columns: true,
+            skip_empty_lines: true,
+          });
+
+        return parsed;
+    })
+
+
+    eleventyConfig.addShortcode("image", async function (src, alt, sizes) {
+		let metadata = await Image(src, {
+			widths: [350, "auto"],
+			formats: ["avif", "jpeg"],
+		});
+
+
+		let imageAttributes = {
+			alt,
+			sizes,
+			loading: "lazy",
+			decoding: "async",
+		};
+
+        let options = {
+            outputDir: "/img/",
+            urlPath: "/img/"
+        }
+
+		// You bet we throw an error on a missing alt (alt="" works okay)
+		return Image.generateHTML(metadata, imageAttributes, options);
+    })
+
+
+    eleventyConfig.addShortcode("galleryImage", async function (src, alt) {
+        let options = {
+            outputDir: "/img/",
+            urlPath: "/img/"
+        }
+
+		let metadata = await Image(src, {
+			widths: [350, "auto"],
+			formats: ["avif"],
+		}, options);
+
+
+        thumbnail = metadata['avif'][0]
+        fullsize = metadata['avif'][1]
+
+        return `<a href="${fullsize.url}" 
+        data-pswp-width="${fullsize.width}" 
+        data-pswp-height="${fullsize.height}" 
+        target="_blank">
+            <img src="${thumbnail.url}" alt="${alt}" />
+        </a>`
+
+      })
+
 
     eleventyConfig.addFilter('htmlDateString', (dateObj) => {
         // dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
@@ -24,6 +84,7 @@ module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy({ 
         "_static/": "/",
+        "img": "/img/",
         "node_modules/photoswipe/dist/photoswipe-lightbox.esm.min.js": "assets/photoswipe-lightbox.esm.min.js",
         "node_modules/photoswipe/dist/photoswipe.esm.min.js": "assets/photoswipe.esm.min.js",
         "node_modules/photoswipe/dist/photoswipe.css": "assets/photoswipe.css"
